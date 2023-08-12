b3ec2209119cacc058eb097f0affcd4404c6eb66
p-adenis <p.antoine.denis@gmail.com>
2023-08-12 14:44:13 +0200

    Create function initialise_dict() to create empty dict

--- main.py
+++ main.py
@@ -8,26 +8,24 @@ def check_number(integer):
     else:
         return False
     
-    if 
+def initialise_dict():
+    """
+        Function to create and initialise an new empty dictionnary with 20 values
+    """
 
+    liste_nombre = {}
+
+    for i in range(1,21):
+        key = 'rang ' + str(i)
+        value = ''
+        liste_nombre[key] = value
+    
+    print(liste_nombre)
+    
 def main():
 
-    nombres = {}
-
-    for i in range(20):
-        key = 'rang' + str(i)
-        value = ''
-        nombres[key] = value
-
-        rand_numb = random.randint(0,1000)
-
-        choice = input('Choose rank to put this number')
-
-
-        if choice is not None and int(choice) in range(1,21):
-            print(choice,rand_numb)
-        else:
-            print("Please choose a rank between [1-20] !! ")
+  
+    initialise_dict()
 
 if __name__ == '__main__':
     main()
